c18d13439d2615f87f189707957479f679c0f390
prashant7798 <prkamble79@gmail.com>
2022-08-04 14:42:51 +0530

    My frist project

--- src/GitRepo/GitTest1.java
+++ src/GitRepo/GitTest1.java
@@ -2,8 +2,16 @@ package GitRepo;
 
 public class GitTest1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+			System.out.println("eat somethig");
+		else
+		{
+			System.out.println("do work---");
+		}
 
 	}
 
